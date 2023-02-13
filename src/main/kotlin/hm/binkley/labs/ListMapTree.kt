7b95d70ec749f7f99c554f470c17efd04496ba2f
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2023-02-13 07:38:01 -0600

    Children

--- src/main/kotlin/hm/binkley/labs/ListMapTree.kt
+++ src/main/kotlin/hm/binkley/labs/ListMapTree.kt
@@ -1,3 +1,9 @@
 package hm.binkley.labs
 
-class ListMapTree(val name: String)
+class ListMapTree private constructor(val name: String, val depth: Int) {
+    fun newChild(name: String) = ListMapTree(name, depth + 1)
+
+    companion object {
+        fun newRoot(name: String) = ListMapTree(name, 0)
+    }
+}
--- src/test/kotlin/hm/binkley/labs/ListMapTreeTest.kt
+++ src/test/kotlin/hm/binkley/labs/ListMapTreeTest.kt
@@ -6,8 +6,31 @@ import org.junit.jupiter.api.Test
 internal class ListMapTreeTest {
     @Test
     fun `should make a tree`() {
-        val tree = ListMapTree("ROOT")
+        val tree = ListMapTree.newRoot("ROOT")
 
         tree.name shouldBe "ROOT"
+    }
+
+    @Test
+    fun `root depth should be 0`() {
+        val tree = ListMapTree.newRoot("ROOT")
+
+        tree.depth shouldBe 0
+    }
+
+    @Test
+    fun `should create a child`() {
+        val tree = ListMapTree.newRoot("ROOT")
+        val child = tree.newChild("CHILD")
+
+        child.name shouldBe "CHILD"
+    }
+
+    @Test
+    fun `first child depth should be 1`() {
+        val tree = ListMapTree.newRoot("ROOT")
+        val child = tree.newChild("CHILD")
+
+        child.depth shouldBe 1
     }
 }
