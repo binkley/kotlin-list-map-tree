f9c16efdfe7bf45c955719cc934df1e8f8c77df2
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2023-02-14 06:51:10 -0600

    True tests
    Coverage needs raising.

--- src/main/kotlin/hm/binkley/labs/ListMapTree.kt
+++ src/main/kotlin/hm/binkley/labs/ListMapTree.kt
@@ -1,7 +1,29 @@
 package hm.binkley.labs
 
-class ListMapTree private constructor(val name: String, val depth: Int) {
-    fun newChild(name: String) = ListMapTree(name, depth + 1)
+import java.util.Objects.hash
+import java.util.TreeSet
+
+class ListMapTree private constructor(val name: String, val depth: Int) :
+    Comparable<ListMapTree> {
+    private val _children = TreeSet<ListMapTree>()
+    val children get() = _children.toList()
+
+    fun newChild(name: String) = ListMapTree(name, depth + 1).also {
+        _children.add(it)
+    }
+
+    override fun compareTo(other: ListMapTree) = name.compareTo(other.name)
+
+    override fun equals(other: Any?): Boolean = this === other ||
+        other is ListMapTree &&
+        name == other.name &&
+        depth == other.depth &&
+        _children == other._children
+
+    override fun hashCode() = hash(name, depth, _children)
+
+    override fun toString() =
+        "ListMapTree[name=$name, depth=$depth, children=$_children]"
 
     companion object {
         fun newRoot(name: String) = ListMapTree(name, 0)
--- src/test/kotlin/hm/binkley/labs/ListMapTreeTest.kt
+++ src/test/kotlin/hm/binkley/labs/ListMapTreeTest.kt
@@ -1,36 +1,91 @@
 package hm.binkley.labs
 
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import org.junit.jupiter.api.Test
 
 internal class ListMapTreeTest {
     @Test
-    fun `should make a tree`() {
-        val tree = ListMapTree.newRoot("ROOT")
+    fun `should make a root`() {
+        val root = ListMapTree.newRoot("ROOT")
 
-        tree.name shouldBe "ROOT"
+        root.name shouldBe "ROOT"
     }
 
     @Test
     fun `root depth should be 0`() {
-        val tree = ListMapTree.newRoot("ROOT")
+        val root = ListMapTree.newRoot("ROOT")
 
-        tree.depth shouldBe 0
+        root.depth shouldBe 0
+    }
+
+    @Test
+    fun `equality and hashing should consider name`() {
+        val rootA = ListMapTree.newRoot("A-ROOT")
+        val rootB = ListMapTree.newRoot("B-ROOT")
+
+        rootA.equals(rootB) shouldBe false
+        rootA.hashCode() shouldNotBe rootB.hashCode()
+
+        val rootC = ListMapTree.newRoot("ROOT")
+        val rootD = ListMapTree.newRoot("ROOT")
+
+        rootC.equals(rootD) shouldBe true
+        rootC.hashCode() shouldBe rootD.hashCode()
+    }
+
+    @Test
+    fun `equality and hashing should consider depth`() {
+        val rootA = ListMapTree.newRoot("ROOT")
+        val child = rootA.newChild("ROOT")
+
+        child.newChild("ROOT")
+
+        rootA.equals(child) shouldBe false
+        rootA.hashCode() shouldNotBe child.hashCode()
+    }
+
+    @Test
+    fun `equality and hashing should consider children`() {
+        val rootA = ListMapTree.newRoot("ROOT")
+        val rootB = ListMapTree.newRoot("ROOT")
+
+        rootA.newChild("CHILD")
+
+        rootA.equals(rootB) shouldBe false
+        rootA.hashCode() shouldNotBe rootB.hashCode()
+
+        val rootC = ListMapTree.newRoot("ROOT")
+        val rootD = ListMapTree.newRoot("ROOT")
+
+        rootC.newChild("CHILD")
+        rootD.newChild("CHILD")
+
+        rootC.equals(rootD) shouldBe true
+        rootC.hashCode() shouldBe rootD.hashCode()
     }
 
     @Test
     fun `should create a child`() {
-        val tree = ListMapTree.newRoot("ROOT")
-        val child = tree.newChild("CHILD")
+        val root = ListMapTree.newRoot("ROOT")
+        val child = root.newChild("CHILD")
 
         child.name shouldBe "CHILD"
     }
 
     @Test
     fun `first child depth should be 1`() {
-        val tree = ListMapTree.newRoot("ROOT")
-        val child = tree.newChild("CHILD")
+        val root = ListMapTree.newRoot("ROOT")
+        val child = root.newChild("CHILD")
 
         child.depth shouldBe 1
+    }
+
+    @Test
+    fun `root should remember children`() {
+        val root = ListMapTree.newRoot("ROOT")
+        val child = root.newChild("CHILD")
+
+        root.children shouldBe listOf(child)
     }
 }
